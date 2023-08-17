d78db01b02846639aa355a1d4cc11d61bcc8c265
Nicholas Cathcart <nicholascathcart@nicholass-air.lan>
2023-08-17 00:56:05 -0400

    File Explorer fundamentals are done. Need to make it pretty.

--- component-library/src/app/file-explorer/page.tsx
+++ component-library/src/app/file-explorer/page.tsx
@@ -1,7 +1,29 @@
-import FileExplorer from "../../components/FileExplorer"
+import FileExplorer, { FileSystem }from "../../components/FileExplorer"
 
 export default function FileExplorerPage(){
+    const exampleFiles: FileSystem = {
+        name: "root",
+        isDirectory: true,
+        children: [
+            {
+                name: "src",
+                isDirectory: true,
+                children: [
+                    {
+                        name: "main.ts", isDirectory: false
+                    },
+                    {
+                        name: ".gitignore", isDirectory: false
+                    }
+                ]
+            },
+            {
+                name: "package.json",
+                isDirectory: false,
+            }
+        ]
+    }
     return (
-        <FileExplorer/>
+        <FileExplorer files={exampleFiles}/>
     )
 }
--- component-library/src/components/FileExplorer.tsx
+++ component-library/src/components/FileExplorer.tsx
@@ -1,36 +1,67 @@
-import styles from "./FileExplorer.module.css"
+"use client"
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import styles from "./FileExplorer.module.css";
+import { motion } from "framer-motion";
 
-interface FileSystem {
-    name: string;
-    isDirectory: boolean;
-    children?: FileSystem[];
+export interface FileSystem {
+  name: string;
+  isDirectory: boolean;
+  children?: FileSystem[];
+}
+interface Props {
+  files: FileSystem;
 }
 
-export default function FileExplorer(){
-
-    const exampleFiles: FileSystem = {
-        name: "root",
-        isDirectory: true,
-        children: [
-            {
-                name: "src",
-                isDirectory: true,
-                children: [
-                    {
-                        name: "main.ts", isDirectory: false
-                    },
-                    {
-                        name: ".gitignore", isDirectory: false
-                    }
-                ]
-            },
-            {
-                name: "package.json",
-                isDirectory: false,
-            }
-        ]
+export default function FileExplorer({ files }: Props) {
+  const [expand, setExpand] = useState(false);
+  const variants = {
+    enter: {
+      
+        height: "0px",
+        opacity: 0,
+        transition: { duration: 0.4 },
+      
+    },
+    center: () => {
+        return {
+          height: expand  ? "auto" : "0px",
+          opacity: 1,
+          transition: { duration: 0.4 },
+        };
+      },
+    exit: {
+        height: "0px",
+        opacity: 0,
+        transition: { duration: 0.4 },
     }
-    return (
-        <div>This is the File Explorer</div>
-    )
+  };
+  return (
+    <motion.div className={styles.fileExplorerContainer} layout>
+      <div className={styles.directory}>
+        <button onClick={()=>setExpand(!expand)}>clickMe</button>
+        {files.name}
+      </div>
+      <motion.div
+        className={styles.childrenContainer}
+        variants={variants}
+        initial={"enter"}
+        animate={"center"}
+        exit={"exit"}
+        layout
+      >
+      {files.children?.map((child) => {
+        if (child.isDirectory) {
+          return <FileExplorer key={uuid()} files={child} />;
+        } else {
+          return (
+            <div key={uuid()} className={styles.file}>
+              {child.name}
+            </div>
+          );
+        }
+      })}
+      </motion.div>
+    </motion.div>
+  );
 }
