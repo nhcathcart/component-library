6a2f804d69c76f31a8c61842be081fbf5e14d6b7
Nicholas Cathcart <nicholascathcart@nicholass-air.lan>
2023-08-23 11:01:14 -0400

    file upload working, moving to animating and styling.

--- component-library/src/components/FileUpload.tsx
+++ component-library/src/components/FileUpload.tsx
@@ -1,11 +1,39 @@
-import styles from "./FileUpload.module.css"
-import Dropzone from "dropzone"
-export default function FileUpload(){
-    
-    return (
-        <div className={styles.fileUploadContainer}>
-            <input type={"file"}/>
-            <div id={styles.dropZone}>Drop your files here</div>
-        </div>
-    )
+"use client"
+import React, { ChangeEvent, useState } from "react";
+import styles from "./FileUpload.module.css";
+
+export default function FileUpload() {
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+
+  function handleFileInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const files = event.target.files;
+  
+    if (files !== null) {
+      const selectedFiles = Array.from(files);
+      setSelectedFiles(selectedFiles);
+    }
+  }
+
+  return (
+    <div className={styles.fileUploadContainer}>
+      <label htmlFor="fileInput" className={styles.customFileInput}>
+        Choose file
+      </label>
+      <input
+        type="file"
+        style={{ display: "none" }}
+        id="fileInput"
+        multiple={true}
+        onChange={handleFileInputChange}
+      />
+      <div className={styles.fileListContainer}>
+        <h4>Selected Files:</h4>
+        <ul>
+          {selectedFiles.map((file, index) => (
+            <li key={index}>{file.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
