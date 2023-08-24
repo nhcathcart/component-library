8b82e04fbf162089f8686fa60211bf295f8c5c16
Nicholas Cathcart <nicholascathcart@nicholass-air.lan>
2023-08-24 11:58:36 -0400

    cleaned up styling on paper-tabs

--- component-library/src/components/Tabs.tsx
+++ component-library/src/components/Tabs.tsx
@@ -1,33 +1,44 @@
-"use client"
+"use client";
 import { ReactNode, useState } from "react";
-import styles from "./Tabs.module.css"
+import styles from "./Tabs.module.css";
 
 interface Props {
-    content: Tab[]
+  content: Tab[];
 }
 interface Tab {
-    title: string;
-    content: ReactNode;
+  title: string;
+  content: ReactNode;
 }
-export default function Tabs ({content}: Props) {
-    const [activeIndex, setActiveIndex] = useState(0)
-    function chooseTab(index: number){
-        return setActiveIndex(index);
-    }
-    return (
-        <div className={styles.tabsContainer}>
-            <div className={styles.tabTopsContainer}>
-                {content.map((item, index) => {
-                    return (
-                        <div key={`tab-${index}`} className={index === activeIndex? styles.tabTopActive : styles.tabTop} onClick={() => chooseTab(index)}>{item.title}</div>
-                    )
-                })}
-                
+export default function Tabs({ content }: Props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  function chooseTab(index: number) {
+    return setActiveIndex(index);
+  }
+  const tabSize = 100 / content.length;
+  
+  return (
+    <div className={styles.tabsContainer}>
+      <div className={styles.tabTopsContainer}>
+        {content.map((item, index) => {
+          return (
+            <div
+              key={`tab-${index}`}
+              className={
+                index === activeIndex ? styles.tabTopActive : styles.tabTop
+              }
+              onClick={() => chooseTab(index)}
+              style={{
+                width: `${tabSize}`
+              }}
+            >
+              {item.title}
             </div>
-            <div className={styles.tabsContentContainer}>
-                {content[activeIndex].content}
-            </div>
-            
-        </div>
-    )
+          );
+        })}
+      </div>
+      <div className={styles.tabsContentContainer}>
+        {content[activeIndex].content}
+      </div>
+    </div>
+  );
 }
