851f23378529184691ee4c7ebbbb273f40517253
Nicholas Cathcart <nicholascathcart@nicholass-air.lan>
2023-08-23 22:40:20 -0400

    updated homepage text

--- component-library/src/app/page.tsx
+++ component-library/src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div>
-      Hello there
+      Welcome! This a place to hold some components I&apos;ve been working on. Feel free to use them as you see fit. They are set up to be used next.js applications, but they can easily be modified ( in most cases just remove the &quot;use client&quot;). They do need to be used with CSS modules, or there will very likely be some collisions.
     </div>
   )
 }
